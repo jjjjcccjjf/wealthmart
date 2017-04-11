2f63b901b6a75560767661fd381ebc50b13dc043
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-04-11 10:44:32 +0800

    add uploading of file

--- wp-content/themes/listify/advisor-account.php
+++ wp-content/themes/listify/advisor-account.php
@@ -5,8 +5,32 @@ Template Name: Advisor Account
 
 global $style;
 
+
 if($_POST){
 	$detail_data = array();
+
+	/**
+	* if there is a file
+	*/
+	if($_FILES['listing_photo']['size'] > 0){
+		$uploaddir = wp_upload_dir()['path'] . '/';
+
+		$fn = explode('.', basename($_FILES['listing_photo']['name']));
+		$fn[0] .= time();
+
+		$new_file_name = implode('.', $fn);
+
+		$file = $uploaddir . $new_file_name;
+		$listing_photo_url = wp_upload_dir()['url'] . '/' . $new_file_name;
+
+		$raw_file_name = $_FILES['listing_photo']['tmp_name'];
+		if (move_uploaded_file($_FILES['listing_photo']['tmp_name'], $file)) {
+			# TODO: add some kind of confirmation shizzz
+		} else {
+			// echo "error";
+		}
+		$detail_data['listing_photo'] = $listing_photo_url;
+	}
 	$detail_data['ID'] = $GLOBALS['current_user']->ID;
 	$detail_data['position'] = $_POST['position'];
 	$detail_data['address'] = $_POST['address'];
@@ -71,7 +95,7 @@ $style = 'grid-standard' == $blog_style ? 'standard' : 'cover';
 get_header();
 
 if($GLOBALS['current_user']->roles[0] == 'pending_vendor'){
-	$cudata = $wpdb->get_row('SELECT * FROM advisor_details WHERE ID = '.$GLOBALS['current_user']->ID, ARRAY_A);
+	$advisor_details = $wpdb->get_row('SELECT * FROM advisor_details WHERE ID = '.$GLOBALS['current_user']->ID, ARRAY_A);
 }elseif($GLOBALS['current_user']->roles[0] == 'customer'){
 	// dont show this to end users
 	?>
@@ -116,18 +140,18 @@ $last_name = get_user_meta($GLOBALS['current_user']->ID, 'last_name', true);
 $name = $first_name . " " . $last_name;
 
 ?>
-<form method="POST">
+<form method="POST" enctype="multipart/form-data">
 	<div class="container">
 		<section class="listing-banner" style="background: url(<?php echo theme_url; ?>images/bannerimg.jpg) no-repeat center center; background-size: cover;">
 			<aside>
 				<h1><?php echo $name ?>
 					&nbsp;
-					<?php if($cudata['is_verified']):?>
+					<?php if($advisor_details['is_verified']):?>
 						<span>Verified <i class="fa fa-check" aria-hidden="true"></i></span>
 					<?php endif;?>
 				</h1>
-				<h4><?php echo $cudata['address'] ?></h4>
-				<p><?php if($cudata){ echo str_replace(',', ', ', $cudata['expertise']); } ?></p>
+				<h4><?php echo $advisor_details['address'] ?></h4>
+				<p><?php if($advisor_details){ echo str_replace(',', ', ', $advisor_details['expertise']); } ?></p>
 				<fieldset class="rating">
 					<input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
 					<input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
@@ -147,228 +171,231 @@ $name = $first_name . " " . $last_name;
 		<section class="listing-details">
 			<aside class="sidebar">
 				<div class="profile-pic">
-					<img src="images/profile-pic.jpg">
-				</div>
-				<div class="agent-details">
-					<ul>
-						<li><i class="fa fa-user" aria-hidden="true"></i>
-							<input type="text" name="position" placeholder="Position" <?php if($cudata){ ?>value="<?php echo $cudata['position']; ?>"<?php } ?>>
-						</li>
-						<li><i class="fa fa-map-marker" aria-hidden="true"></i>
-							<input type="text" placeholder="Address" name="address" <?php if($cudata){ ?>value="<?php echo $cudata['address']; ?>"<?php } ?> >
-						</li>
-						<li><i class="fa fa-phone" aria-hidden="true"></i>
-							<input type="text" placeholder="Contact Number" name="contact" <?php if($cudata){ ?>value="<?php echo $cudata['contact']; ?>"<?php } ?>>
-						</li>
-						<li><i class="fa fa-envelope" aria-hidden="true"></i>
-							<?php echo $GLOBALS['current_user']->user_email; ?>
-						</li>
-					</ul>
+					<?php if(isset($advisor_details['listing_photo'])){
+						?><img src="<?php echo $advisor_details['listing_photo']; ?>" >
+						<?php } ?>
+						<input type="file" name="listing_photo">
+					</div>
+					<div class="agent-details">
+						<ul style="margin-top:80px;">
+							<li><i class="fa fa-user" aria-hidden="true"></i>
+								<input type="text" name="position" placeholder="Position" <?php if($advisor_details){ ?>value="<?php echo $advisor_details['position']; ?>"<?php } ?>>
+							</li>
+							<li><i class="fa fa-map-marker" aria-hidden="true"></i>
+								<input type="text" placeholder="Address" name="address" <?php if($advisor_details){ ?>value="<?php echo $advisor_details['address']; ?>"<?php } ?> >
+							</li>
+							<li><i class="fa fa-phone" aria-hidden="true"></i>
+								<input type="text" placeholder="Contact Number" name="contact" <?php if($advisor_details){ ?>value="<?php echo $advisor_details['contact']; ?>"<?php } ?>>
+							</li>
+							<li><i class="fa fa-envelope" aria-hidden="true"></i>
+								<?php echo $GLOBALS['current_user']->user_email; ?>
+							</li>
+						</ul>
 
-				</div>
-			</aside>
+					</div>
+				</aside>
 
-			<article class="agent-main">
+				<article class="agent-main">
 
-				<h3 class="experience-label">
-					Experience
-					<!-- <button>
-					<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
-				</button>
-				<button>
-				<i class="fa fa-plus-square" aria-hidden="true"></i> Edit
-			</button> -->
-		</h3>
-		<section class="agent-content">
-			<textarea name="experience"><?php if($cudata){ echo $cudata['experience']; } ?></textarea>
-		</section>
-
-		<hr>
-
-		<h3 class="licenses-label">
-			Licenses, Certifications and Affiliations
-			<!--<button>
-			<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
-		</button>
-		<button>
-		<i class="fa fa-plus-square" aria-hidden="true"></i> Edit
-	</button> -->
-</h3>
-
-<section class="agent-content">
-	<?php
-	$licenses = array('Licensed Insurance Advisor', 'Certified Investment Solicitor (CIS)', 'Registered Financial Planner (RFP)', 'Registered Financial Consultant (RFC)', 'Certified Wealth Planner (CWP)', 'Registered Estate Planner (REP)', 'Chartered Wealth Manager (CWM)', 'Accredited Financial Analyst (AFA)', 'Chartered Trust and Estate Planner (CTEP)', 'Certified Securities Representative (CSR)', 'Certified Treasuries Professional (CTP)', 'Life Underwriters Training Council Fellow (LUTCF)', 'Chartered Financial Planner (ChFP)', 'Chartered Financial Analyst (CFA)', 'Certified Public Accountant (CPA)', 'International Accredited Accountant (IAA)', 'Others, pls specify');
-
-	$other_license = "";
-	$user_licenses_arr = array();
-	if($cudata){
-		$user_licenses = explode(",", $cudata['license']);
-		foreach($user_licenses as $user_license){
-			if(in_array($user_license, $licenses)){
-				$user_licenses_arr[] = $user_license;
-			}else{
-				if(!in_array("Others", $user_licenses_arr)){
-					$user_licenses_arr[] = "Others";
-				}
-
-				if(empty($other_license)){
-					$other_license = $user_license;
-				}else{
-					$other_license .= ",".$user_license;
-				}
-			}
-		}
-	}
-	?>
-	<ul class="listbox">
-		<?php
-		foreach($licenses as $license){
-			?>
-			<li>
-				<input type="checkbox" name="license[]" value="<?php echo $license; ?>" <?php if(($license == 'Others, pls specify' && in_array("Others", $user_licenses_arr)) || in_array($license, $user_licenses_arr)){ echo "checked"; } ?>> <?php echo $license; ?> <?php if($license == 'Others, pls specify'){ ?><input type="text" name="license_other" value="<?php echo $other_license; ?>"><?php } ?></li>
-
-				<?php } ?>
-			</ul>
-		</section>
-		<hr>
-
-		<h3 class="expertise-label">
-			Expertise
-			<!-- <button>
-			<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
-		</button>
-		<button>
-		<i class="fa fa-plus-square" aria-hidden="true"></i> Edit
-	</button> -->
-</h3>
-<section class="agent-content">
-	<?php
-	$expertise = array('Personal Finance Planning Consultation','Corporate / In-house Training','Investment Planning','Insurance Planning','Retirement Planning','Trust and Estate Planning','Education Planning', 'Others, pls specify');
-
-	$other_expertise = "";
-	$user_expertise_arr = array();
-	if($cudata){
-		$user_expertise = explode(",", $cudata['expertise']);
-		foreach($user_expertise as $user_field){
-			if(in_array($user_field, $expertise)){
-				$user_expertise_arr[] = $user_field;
-			}else{
-				if(!in_array("Others", $user_expertise_arr)){
-					$user_expertise_arr[] = "Others";
-				}
-
-				if(empty($other_expertise)){
-					$other_expertise = $user_field;
-				}else{
-					$other_expertise .= ",".$user_field;
-				}
-			}
-		}
-	}
-	?>
-	<ul class="listbox">
-		<?php
-		foreach($expertise as $field){
-			?>
-			<li>
-				<input type="checkbox" name="expertise[]" value="<?php echo $field; ?>"
-				<?php if(
-					($field == 'Others, pls specify' && in_array("Others", $user_expertise_arr)) ||
-					in_array($field, $user_expertise_arr)
-				){ echo "checked"; } ?>>
-				<?php echo $field; ?>
-				<?php if($field == 'Others, pls specify'){
-					?><input type="text" name="expert_other" value="<?php echo $other_expertise; ?>">
-					<?php } ?>
-				</li>
-
-				<?php } ?>
+					<h3 class="experience-label">
+						Experience
+						<!-- <button>
+						<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
+					</button>
+					<button>
+					<i class="fa fa-plus-square" aria-hidden="true"></i> Edit
+				</button> -->
+			</h3>
+			<section class="agent-content">
+				<textarea name="experience"><?php if($advisor_details){ echo $advisor_details['experience']; } ?></textarea>
 			</section>
+
 			<hr>
 
-			<h3 class="awards-label">
-				Awards and Recognitions
+			<h3 class="licenses-label">
+				Licenses, Certifications and Affiliations
 				<!--<button>
 				<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
 			</button>
 			<button>
 			<i class="fa fa-plus-square" aria-hidden="true"></i> Edit
-		</button>-->
+		</button> -->
+	</h3>
+
+	<section class="agent-content">
+		<?php
+		$licenses = array('Licensed Insurance Advisor', 'Certified Investment Solicitor (CIS)', 'Registered Financial Planner (RFP)', 'Registered Financial Consultant (RFC)', 'Certified Wealth Planner (CWP)', 'Registered Estate Planner (REP)', 'Chartered Wealth Manager (CWM)', 'Accredited Financial Analyst (AFA)', 'Chartered Trust and Estate Planner (CTEP)', 'Certified Securities Representative (CSR)', 'Certified Treasuries Professional (CTP)', 'Life Underwriters Training Council Fellow (LUTCF)', 'Chartered Financial Planner (ChFP)', 'Chartered Financial Analyst (CFA)', 'Certified Public Accountant (CPA)', 'International Accredited Accountant (IAA)', 'Others, pls specify');
+
+		$other_license = "";
+		$user_licenses_arr = array();
+		if($advisor_details){
+			$user_licenses = explode(",", $advisor_details['license']);
+			foreach($user_licenses as $user_license){
+				if(in_array($user_license, $licenses)){
+					$user_licenses_arr[] = $user_license;
+				}else{
+					if(!in_array("Others", $user_licenses_arr)){
+						$user_licenses_arr[] = "Others";
+					}
+
+					if(empty($other_license)){
+						$other_license = $user_license;
+					}else{
+						$other_license .= ",".$user_license;
+					}
+				}
+			}
+		}
+		?>
+		<ul class="listbox">
+			<?php
+			foreach($licenses as $license){
+				?>
+				<li>
+					<input type="checkbox" name="license[]" value="<?php echo $license; ?>" <?php if(($license == 'Others, pls specify' && in_array("Others", $user_licenses_arr)) || in_array($license, $user_licenses_arr)){ echo "checked"; } ?>> <?php echo $license; ?> <?php if($license == 'Others, pls specify'){ ?><input type="text" name="license_other" value="<?php echo $other_license; ?>"><?php } ?></li>
+
+					<?php } ?>
+				</ul>
+			</section>
+			<hr>
+
+			<h3 class="expertise-label">
+				Expertise
+				<!-- <button>
+				<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
+			</button>
+			<button>
+			<i class="fa fa-plus-square" aria-hidden="true"></i> Edit
+		</button> -->
 	</h3>
 	<section class="agent-content">
-		<textarea name="award"><?php if($cudata){ echo $cudata['award']; } ?></textarea>
-	</section>
-	<hr>
+		<?php
+		$expertise = array('Personal Finance Planning Consultation','Corporate / In-house Training','Investment Planning','Insurance Planning','Retirement Planning','Trust and Estate Planning','Education Planning', 'Others, pls specify');
 
-	<h3 class="gallery-label">
-		Photo Gallery
-		<!--<button>
-		<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
-	</button>
-	<button>
-	<i class="fa fa-minus-square" aria-hidden="true"></i> Delete
-</button>-->
-</h3>
-<div class="agent-pgallery">
-	<div class="carousel" data-flickity='{ "wrapAround": true, "groupCells": true, "autoPlay": true }'>
-		<div class="carousel-cell"><img src="images/thumb1.jpg"></div>
-		<div class="carousel-cell"><img src="images/thumb2.jpg"></div>
-		<div class="carousel-cell"><img src="images/thumb1.jpg"></div>
-		<div class="carousel-cell"><img src="images/thumb2.jpg"></div>
-		<div class="carousel-cell"><img src="images/thumb1.jpg"></div>
-		<div class="carousel-cell"><img src="images/thumb2.jpg"></div>
-		<div class="carousel-cell"><img src="images/thumb1.jpg"></div>
-		<div class="carousel-cell"><img src="images/thumb2.jpg"></div>
-	</div>
-</div>
+		$other_expertise = "";
+		$user_expertise_arr = array();
+		if($advisor_details){
+			$user_expertise = explode(",", $advisor_details['expertise']);
+			foreach($user_expertise as $user_field){
+				if(in_array($user_field, $expertise)){
+					$user_expertise_arr[] = $user_field;
+				}else{
+					if(!in_array("Others", $user_expertise_arr)){
+						$user_expertise_arr[] = "Others";
+					}
 
-<hr>
+					if(empty($other_expertise)){
+						$other_expertise = $user_field;
+					}else{
+						$other_expertise .= ",".$user_field;
+					}
+				}
+			}
+		}
+		?>
+		<ul class="listbox">
+			<?php
+			foreach($expertise as $field){
+				?>
+				<li>
+					<input type="checkbox" name="expertise[]" value="<?php echo $field; ?>"
+					<?php if(
+						($field == 'Others, pls specify' && in_array("Others", $user_expertise_arr)) ||
+						in_array($field, $user_expertise_arr)
+					){ echo "checked"; } ?>>
+					<?php echo $field; ?>
+					<?php if($field == 'Others, pls specify'){
+						?><input type="text" name="expert_other" value="<?php echo $other_expertise; ?>">
+						<?php } ?>
+					</li>
 
+					<?php } ?>
+				</section>
+				<hr>
 
-<h3 class="video-label">
-	Video Gallery
-	<button>
-		<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
-	</button>
-	<button>
-		<i class="fa fa-minus-square" aria-hidden="true"></i> Delete
-	</button>
-</h3>
-<div class="agent-vgallery">
-	<div class="videocarousel" data-flickity='{ "wrapAround": true }'>
+				<h3 class="awards-label">
+					Awards and Recognitions
+					<!--<button>
+					<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
+				</button>
+				<button>
+				<i class="fa fa-plus-square" aria-hidden="true"></i> Edit
+			</button>-->
+		</h3>
+		<section class="agent-content">
+			<textarea name="award"><?php if($advisor_details){ echo $advisor_details['award']; } ?></textarea>
+		</section>
+		<hr>
 
-		<div class="carousel-cell">
-			<div class="video-container">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/L6CKuz5a65I" frameborder="0" allowfullscreen></iframe>
+		<h3 class="gallery-label">
+			Photo Gallery
+			<button>
+				<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
+			</button>
+			<button>
+				<i class="fa fa-minus-square" aria-hidden="true"></i> Delete
+			</button>
+		</h3>
+		<div class="agent-pgallery">
+			<div class="carousel" data-flickity='{ "wrapAround": true, "groupCells": true, "autoPlay": true }'>
+				<div class="carousel-cell"><img src="images/thumb1.jpg"></div>
+				<div class="carousel-cell"><img src="images/thumb2.jpg"></div>
+				<div class="carousel-cell"><img src="images/thumb1.jpg"></div>
+				<div class="carousel-cell"><img src="images/thumb2.jpg"></div>
+				<div class="carousel-cell"><img src="images/thumb1.jpg"></div>
+				<div class="carousel-cell"><img src="images/thumb2.jpg"></div>
+				<div class="carousel-cell"><img src="images/thumb1.jpg"></div>
+				<div class="carousel-cell"><img src="images/thumb2.jpg"></div>
 			</div>
 		</div>
-		<div class="carousel-cell">
-			<div class="video-container">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/ZNebSeFVPNc" frameborder="0" allowfullscreen></iframe>
+
+		<hr>
+
+
+		<h3 class="video-label">
+			Video Gallery
+			<button>
+				<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
+			</button>
+			<button>
+				<i class="fa fa-minus-square" aria-hidden="true"></i> Delete
+			</button>
+		</h3>
+		<div class="agent-vgallery">
+			<div class="videocarousel" data-flickity='{ "wrapAround": true }'>
+
+				<div class="carousel-cell">
+					<div class="video-container">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/L6CKuz5a65I" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+				<div class="carousel-cell">
+					<div class="video-container">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/ZNebSeFVPNc" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+				<div class="carousel-cell">
+					<div class="video-container">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/vpYkz5WU1Vg" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+
 			</div>
 		</div>
-		<div class="carousel-cell">
-			<div class="video-container">
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/vpYkz5WU1Vg" frameborder="0" allowfullscreen></iframe>
-			</div>
-		</div>
-
-	</div>
-</div>
 
 
-<hr>
+		<hr>
 
-<h3>Rates per Consultation
-	<!--<button>
-	<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
-</button>
-<button>
-<i class="fa fa-plus-square" aria-hidden="true"></i> Edit
-</button>-->
+		<h3>Rates per Consultation
+			<!--<button>
+			<i class="fa fa-pencil-square" aria-hidden="true"></i> Add
+		</button>
+		<button>
+		<i class="fa fa-plus-square" aria-hidden="true"></i> Edit
+	</button>-->
 </h3>
 <section class="agent-content">
-	<textarea name="rates"><?php if($cudata){ echo $cudata['rates']; } ?></textarea>
+	<textarea name="rates"><?php if($advisor_details){ echo $advisor_details['rates']; } ?></textarea>
 </section>
 <hr>
 
